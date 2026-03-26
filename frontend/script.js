0aa2be78c7b18dd04045e60ba46a20b279206ba9
25bclb31-lab <25bclb31@kristujayanti.com>
2026-03-26 13:23:10 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -10,10 +10,8 @@ document.getElementById('feedbackForm').addEventListener('submit', async (e) => 
     statusText.innerText = "Sending...";
 
     // PASTE YOUR RENDER LINK HERE
-    const API_URL = "https://rahim2007-2.onrender.com";
+    const API_URL = "https://rahim2007-2.onrender.com//api/feedback";
 
-  form.addEventListener('submit', async function(e) {  // <-- Add 'async' right here!
-    e.preventDefault();
     try {
         const response = await fetch(API_URL, {
             method: 'POST',
@@ -31,4 +29,5 @@ document.getElementById('feedbackForm').addEventListener('submit', async (e) => 
     } catch (error) {
         statusText.innerText = "Error connecting to server.";
     }
+});
 });
